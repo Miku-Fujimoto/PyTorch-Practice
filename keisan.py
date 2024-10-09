535d314452e768daa0d0a8516681fc185f0c7ce5
Miku-Fujimoto <62207763+Miku-Fujimoto@users.noreply.github.com>
2024-10-09 21:30:13 +0900

    入力値ゲット

--- keisan.py
+++ keisan.py
@@ -1,5 +1,10 @@
-count = 9
+unco_price = 9
 previous = 5
+input_count = input("購入するうんこの数を教えてください")
+count = int(input_count)
 
-if count > previous:
-    print (count * previous)
+if count > unco_price:
+    print ("unkoを"+ str(count)+ "個買います")
+
+else:
+    print ("あなたにうんこはありません")
